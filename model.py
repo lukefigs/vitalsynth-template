32e152bf0ed10ff2c256145bc68b4e6de0a1c992
Luke Figlia <41941476+lukefigs@users.noreply.github.com>
2025-08-01 10:54:55 -0400

    Create model.py

---
+++ model.py
@@ -0,0 +1,11 @@
+import torch.nn as nn
+
+class DPLSTMBlock(nn.Module):
+    def __init__(self, input_size=2, hidden_size=64, num_layers=2, output_size=2):
+        super().__init__()
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.linear = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        out, _ = self.lstm(x)
+        return self.linear(out)
